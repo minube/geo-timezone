b34593e7b0bd6c90ae08b822a6c396c68fe0225a
Ana Belén Mejía <abelenmejia@gmail.com>
2017-09-25 17:29:20 +0200

    geos.ini included to the compilation script

--- bin/compile-geos.sh
+++ bin/compile-geos.sh
@@ -3,17 +3,18 @@
 apt-get install php7.0-dev &&\
 apt-get install php7.0-zip &&\
 apt-get install php7.0-xml &&\
-apt-get install php7.0-mbstring &&\
+apt-get install php7.0-mbstring  &&\
 
 curl -s -O http://download.osgeo.org/geos/geos-3.6.1.tar.bz2 &&\
   tar -xjvf geos-3.6.1.tar.bz2 &&\
   cd geos-3.6.1/ &&\
   ./configure --enable-php &&\
   make &&\
-  make install &&\
-  cd ..
+  make install
 
 ldconfig
+
+cd .. &&\
 
 git clone https://git.osgeo.org/gogs/geos/php-geos.git  &&\
   cd php-geos  &&\
@@ -21,3 +22,13 @@ git clone https://git.osgeo.org/gogs/geos/php-geos.git  &&\
   ./configure  &&\
   make  &&\
   make install
+
+cat > /etc/php/7.0/mods-available/geos.ini << EOF
+; configuration for php geos module
+; priority=50
+extension=geos.so
+EOF
+
+cd /etc/php/7.0/cli/conf.d &&\
+ln -s /etc/php/7.0/mods-available/geos.ini &&\
+phpenmod geos
