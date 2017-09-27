30d507958e1dd31c91291dafc7d33d7952803f06
Ana Belén Mejía <abelenmejia@gmail.com>
2017-09-27 10:09:18 +0200

    order bash

--- bin/compile-geos.sh
+++ bin/compile-geos.sh
@@ -3,25 +3,25 @@
 apt-get install php7.0-dev &&\
 apt-get install php7.0-zip &&\
 apt-get install php7.0-xml &&\
-apt-get install php7.0-mbstring  &&\
+apt-get install php7.0-mbstring &&\
 
 curl -s -O http://download.osgeo.org/geos/geos-3.6.1.tar.bz2 &&\
   tar -xjvf geos-3.6.1.tar.bz2 &&\
   cd geos-3.6.1/ &&\
   ./configure --enable-php &&\
   make &&\
-  make install
+  make install &&\
+  cd .. &&\
 
 ldconfig
 
-cd .. &&\
 
 git clone https://git.osgeo.org/gogs/geos/php-geos.git  &&\
   cd php-geos  &&\
   ./autogen.sh  &&\
   ./configure  &&\
   make  &&\
-  make install
+  make install &&\
 
 ls /etc/php &&\
 cat <<EOF > /etc/php/7.0/mods-available/geos.ini
