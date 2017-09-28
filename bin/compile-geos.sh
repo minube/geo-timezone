62c448c6e335c703435aa015c94d1e47c88a00e4
Ana Belén Mejía <abelenmejia@gmail.com>
2017-09-28 11:26:17 +0200

    Updated compilation script and explain the last step of the geos configuration in README file

--- bin/compile-geos.sh
+++ bin/compile-geos.sh
@@ -15,21 +15,21 @@ curl -s -O http://download.osgeo.org/geos/geos-3.6.1.tar.bz2 &&\
 
 ldconfig
 
-
 git clone https://git.osgeo.org/gogs/geos/php-geos.git  &&\
   cd php-geos  &&\
   ./autogen.sh  &&\
   ./configure  &&\
   make  &&\
-  make install &&\
+  make install
 
-ls /etc/php &&\
-cat <<EOF > /etc/php/7.0/mods-available/geos.ini
-; configuration for php geos module
-; priority=50
-extension=geos.so
-EOF
-
-cd /etc/php/7.0/cli/conf.d &&\
-ln -s /etc/php/7.0/mods-available/geos.ini &&\
-phpenmod geos
+# Enable geos module in your system
+# ls /etc/php &&\
+# cat <<EOF > /etc/php/7.0/mods-available/geos.ini
+# ; configuration for php geos module
+# ; priority=50
+# extension=geos.so
+# EOF
+#
+# cd /etc/php/7.0/cli/conf.d &&\
+# ln -s /etc/php/7.0/mods-available/geos.ini &&\
+# phpenmod geos
